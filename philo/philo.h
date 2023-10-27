ab3958b21378522625631d638fd536b704e7ad65
jrocha-v <jrocha-v@student.42porto.com>
2023-10-27 17:16:23 +0100

    init

---
+++ philo/philo.h
@@ -0,0 +1,24 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   philo.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jrocha-v <jrocha-v@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/27 16:47:59 by jrocha-v          #+#    #+#             */
+/*   Updated: 2023/10/27 17:15:54 by jrocha-v         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PHILO_H
+# define PHILO_H
+
+# include <unistd.h>
+# include <stdlib.h>
+
+# define ERROR 1
+# define SUCCESS 0
+
+// fdf_main.c
+
+#endif
